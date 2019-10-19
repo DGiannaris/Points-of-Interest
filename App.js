45f1d73d5add12b5cdde0f7851dca43f81b8a941
DGiananris <mtsgiannaris@gmail.com>
2019-10-19 13:35:28 +0300

    done 1)

--- App.js
+++ App.js
@@ -1,19 +1,176 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './HomeScreen.js'
-export default function App() {
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+  Alert,
+} from 'react-native';
+
+
+
+ function HomeScreen(props) {
   return (
     <View style={styles.container}>
-      <HomeScreen/>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+
+        <Text style={styles.tabBarInfoText}>Welcome to POIS</Text>
+        <TouchableHighlight style={styles.touchableHigh} onPress={() => props.navigation.navigate('List_Map')}>
+          <View><Image style={styles.welcomeImage} source={require('./assets/demap.png')}/></View>
+        </TouchableHighlight>
+      </ScrollView>
+
+
     </View>
   );
 }
+
+function List_MapScreen() {
+
+
+  return (
+    <View style={styles.container}>
+      <Text>banana</Text>
+    </View>
+  );
+}
+
+
+
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    List_Map: List_MapScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+
+
+export default function App() {
+  return (
+    <AppContainer />
+  );
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  imgShadow:{
+    shadowColor: '#202020',
+    shadowOffset: {width: 0, height: 0},
+    shadowRadius: 5,
+  },
+  touchableHigh:{
+      height: 150,
+      width:150,
+      marginTop:100,
+      marginLeft: 105,
+      borderRadius:100,
+      shadowColor: '#202020',
+      shadowOffset: {width: 0, height: 0},
+      shadowRadius: 5,
+      elevation:2,
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop:35,
+    marginLeft: 26,
+
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
   },
 });
